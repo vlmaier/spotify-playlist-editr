354569a091821c26eb669317f6de9affee41f431
Vladas Maier <vladas.maier@goyahealth.de>
2023-05-25 19:57:23 +0200

    Rework code w/ ChatGPT

--- editr.py
+++ editr.py
@@ -1,44 +1,47 @@
 import os
-import schedule
-import spotipy
+import time
 import base64
+import spotipy
+import schedule
 
-from spotipy import SpotifyOAuth
-from dotenv import load_dotenv
 from datetime import datetime
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyOAuth
+
+# Constants
+SCOPE = 'playlist-modify-public ugc-image-upload'
+CACHE_PATH = './cached-token'
+PL_COVER_PATH = "cover.png"
 
 
 def update_playlist():
     load_dotenv()
 
     username = os.getenv('USERNAME')
-    scope = 'playlist-modify-public ugc-image-upload'
     client_id = os.getenv('CLIENT_ID')
     client_secret = os.getenv('CLIENT_SECRET')
     redirect_uri = os.getenv('REDIRECT_URI')
-    cache_path = './cached-token'
-
-    oauth = SpotifyOAuth(username=username,
-                         scope=scope,
-                         client_id=client_id,
-                         client_secret=client_secret,
-                         redirect_uri=redirect_uri,
-                         cache_path=cache_path)
-
-    if os.path.isfile(cache_path):
-        token = oauth.get_cached_token()
-    else:
-        token = oauth.get_access_token()
-
-    sp = spotipy.Spotify(auth=token['access_token'])
 
     pl_id = os.getenv('PLAYLIST_ID')
     pl_title = os.getenv('PLAYLIST_TITLE')
     pl_description = os.getenv('PLAYLIST_DESCRIPTION')
 
-    # ideally 300x300 png file
-    with open("cover.png", "rb") as png_file:
+    with open(PL_COVER_PATH, "rb") as png_file:
         pl_cover = base64.b64encode(png_file.read())
+
+    oauth = SpotifyOAuth(username=username,
+                         scope=SCOPE,
+                         client_id=client_id,
+                         client_secret=client_secret,
+                         redirect_uri=redirect_uri,
+                         cache_path=CACHE_PATH)
+
+    if os.path.isfile(CACHE_PATH):
+        token = oauth.get_cached_token()
+    else:
+        token = oauth.get_access_token()
+
+    sp = spotipy.Spotify(auth=token['access_token'])
 
     sp.playlist_change_details(playlist_id=pl_id, name=pl_title, description=pl_description)
     sp.playlist_upload_cover_image(playlist_id=pl_id, image_b64=pl_cover)
@@ -46,11 +49,14 @@ def update_playlist():
     print("[%s] %s" % (datetime.now(), "playlist updated"))
 
 
-print("[%s] %s" % (datetime.now(), "starting playlist editr ..."))
+if __name__ == "__main__":
+    load_dotenv()
 
-update_playlist()
-schedule.every(1).minutes.do(update_playlist)
+    print("[%s] %s" % (datetime.now(), "starting playlist editr ..."))
 
-while True:
-    schedule.run_pending()
-    continue
+    update_playlist()
+    schedule.every(1).minutes.do(update_playlist)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)  # Delay between executions to avoid high CPU usage
